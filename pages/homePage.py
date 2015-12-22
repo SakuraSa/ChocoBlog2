0ad4667cb72c72b9a0b47b710d096e01a8ec5f3c
SakuraSa <sakura_kyon@hotmail.com>
2015-12-22 13:45:07 +0800

    add pagination template macro, add date box

--- pages/homePage.py
+++ pages/homePage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from flask import render_template
+from flask import render_template, request
 from core.application import app
-from core.models import get_new_session, User, Role, Post
+from core.models import get_new_session, Tag, Post
 
 """
 homePage
@@ -15,7 +15,21 @@ __author__ = 'Rnd495'
 @app.route('/', methods=['GET'])
 def home():
     session = get_new_session()
-    posts = session.query(Post).order_by(Post.post_time).all()
-    html_content = render_template('home.html', posts=posts)
+    sqlite = session.query(Post).order_by(Post.post_time)
+    html_content = render_template('post_list.html', sqlite=sqlite, index=0, size=10)
     session.close()
     return html_content
+
+
+@app.route('/tag/<tag>', methods=['GET'])
+def post_list(tag):
+    session = get_new_session()
+    sqlite = session.query(Post)\
+        .join(Tag)\
+        .filter(Tag.post_id == Post.id and Tag.name == tag)\
+        .order_by(Post.post_time)
+    html_content = render_template('post_list.html', sqlite=sqlite, index=0, size=10)
+    session.close()
+    return html_content
+
+
--- pages/postPage.py
+++ pages/postPage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from flask import render_template
+from flask import render_template, abort
 from core.application import app
+from core.models import get_new_session, Post
 
 """
 postPage
@@ -10,3 +11,11 @@ postPage
 
 __author__ = 'Rnd495'
 
+
+@app.route('/post/<int:post_id>', methods=['GET'])
+def post_show(post_id):
+    session = get_new_session()
+    post = session.query(Post).filter(Post.id == post_id).first()
+    html_content = render_template('post.html', post=post) if post else None
+    session.close()
+    return html_content if html_content else abort(404)
