163ec9bcbea1dfab56b52e5198415c64333071ce
SakuraSa <sakura_kyon@hotmail.com>
2015-12-30 15:53:01 +0800

    change markdown lib form Markdown2 to misaka

--- pages/jinja_filters.py
+++ pages/jinja_filters.py
@@ -2,7 +2,10 @@
 # coding=utf-8
 
 import jinja2
-import markdown2
+import misaka
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_lexer_by_name
 
 from core.application import app
 
@@ -13,12 +16,25 @@ jinja_filters
 __author__ = 'Rnd495'
 
 
-MARKDOWN = markdown2.Markdown(extras=['fenced-code-blocks', 'footnotes', 'tables'])
+class HighlighterRenderer(misaka.HtmlRenderer):
+    def blockcode(self, text, lang):
+        if not lang:
+            return '\n<pre><code>{}</code></pre>\n'.format(text.strip())
+
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = HtmlFormatter()
+
+        return highlight(text, lexer, formatter)
+
+
+MARKDOWN = misaka.Markdown(
+        HighlighterRenderer(),
+        extensions=misaka.EXT_FENCED_CODE | misaka.EXT_TABLES | misaka.EXT_MATH | misaka.EXT_STRIKETHROUGH)
 
 
 @app.template_filter('markdown')
 def safe_markdown(content):
-    return jinja2.Markup(MARKDOWN.convert(content))
+    return jinja2.Markup(MARKDOWN(content))
 
 
 @app.template_filter('datetime_format')
